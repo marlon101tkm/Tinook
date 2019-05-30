b7ce1f660bd8371aba064a40c44528cf365bc6f4
marlon101tkm <50722508+marlon101tkm@users.noreply.github.com>
2019-05-30 17:03:04 -0300

    Add files via upload

--- src/pages/filtro/filtro.js
+++ src/pages/filtro/filtro.js
@@ -3,9 +3,16 @@ import { Combobox } from 'react-widgets'
 
 const nomeAutor = ['joao','pedro'];
 
-const genero=['romance','fantasia','terror','suspense','comdeia','aventura','auto ajuda'];
+const genero=[{id: 1 , nome: 'romance'}
+,
+{id:2 ,nome:'fantasia'},
+{id:3 ,nome:'terror'},
+{id:4 ,nome:'suspense'},
+{id:5 ,nome:'comedia'},
+{id:6 ,nome:'aventura'},
+{id:7 ,nome:'auto ajuda'}];
 
-
+const num = 0;
 
 
 
@@ -15,33 +22,55 @@ class Filtro extends Component{
 
 
 
-     //opAutores = nomeAutor.map((nomeAutor)=> <option>{nomeAutor}</option> )
+     opAutores = nomeAutor.map((nomeAutor)=> <option value = {nomeAutor} >{nomeAutor}</option> )
 
-     //opGenero = genero.map((genero)=><li>{genero}</li>)
+    // opGenero = genero.map((genero)=><option>{genero}</option>)
+
+
+    /* handleSubmit = (e) =>{
+         
+     }*/
 
 
     render(){
         return(
-
+            
             <div>
             
-            
-            
-            <label>Genero:</label>
-            <Combobox placeholder="Genero" data={genero}/>    
-            <label>Autor:</label>
-            <Combobox data={nomeAutor}/> 
+
+          <form >
+          <label>Distancia:</label>
+          <input type="range" />
+          </form>
+           <label>Autor:</label>
+           <select >
+               {this.opAutores}
+           </select>
 
             <label>Idioma:</label>
             <select>
                 <option value='portugues' >Portugues</option>
                 <option value='ingles' >Ingles</option>
             </select><br/>
+
+            
+          
             </div>
+            
 
         )
     }
 }
 
 
+/*
+  <Combobox  valueField='id' textField='nome' data={genero}/> 
+   
+            <label>Genero:</label>   
+           <select>
+               {this.opGenero}
+           </select>
+
+
+ */
 export default Filtro;
